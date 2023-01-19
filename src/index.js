4d33f5aa107422141dc9f771fed8b60378dbd1f6
vitorxp <72953693+vitorxcp@users.noreply.github.com>
2023-01-19 14:26:02 -0300

    update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 // Dependences:
 const Discord = require("discord.js");
 const bot = new Discord.Client({ intents:32767 });
-const Config = require("./aplication/config/index");
+const Config = require("./application/config/index");
 
 //Inicalize events:
-require("./aplication/events/inicialize")(bot)
+require("./application/events/inicialize")(bot)
 
 //Login on application for Discord:
 bot.login(Config.bot.token);
